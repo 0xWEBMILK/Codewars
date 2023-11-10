50b8f32a98e338d45d5d494692a4199ca4e35c5e
Milk Web <webmilk.js@gmail.com>
2023-11-10 07:34:38 -0600

    Task

--- 6_Kyu/Stop gninnipS My sdroW!/index.js
+++ 6_Kyu/Stop gninnipS My sdroW!/index.js
@@ -1,15 +1,5 @@
 const spinWords = (string) => {
-    const result = []
-
     return string.split(' ').map(item => {
-        if (item.length >= 5) {
-            result.length = 0
-            for (let i = item.length; i > -1; i--) {
-                result.push(item[i])
-            }
-        } else {
-            return item
-        }
-        return result.join('')
+        return item.length >= 5 ? item.split('').reverse().join('') : item
     }).join(' ')
 }
