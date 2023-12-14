d08dcf6b7cb44f08135cb8241c11615799ce1977
Milk Web <webmilk.js@gmail.com>
2023-12-14 23:26:30 +0300

    Task

---
+++ JavaScript/6_Kyu/Write Number in Expanded Form/index.js
@@ -0,0 +1,8 @@
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
+
